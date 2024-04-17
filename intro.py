cd11709dc9fcb27167fcfbb27a358a6bb0846589
Stefan Gerstner <Stefan.Gerstner@bib.de>
2024-04-17 12:46:46 +0200

    blank

---
+++ intro.py
@@ -0,0 +1,59 @@
+# In & Out
+x= input()
+print("Eingabe war: " +x)
+
+# Variabeln
+text = "Hello world"
+text = 12
+p=print
+
+# Datentypen
+intVar = 1
+floatVar = 1.0
+boolVar = True
+stringVar = "Wort"
+stringVar = 'Zitate:"Das ist in Anführungszeichen."'
+charVar = 'c'
+
+# Operatoren
+example = 1 + 1 - 2 / 5 * 6
+example2 = 2**3
+example3 = 6//4
+example4 = 6%4
+
+example5 = 5
+example5 += 3
+example5 /= 2
+example5 **= 2
+
+# if, for, while
+if text==12:
+    print("Ist 12")
+elif text>13:
+    print("Ist 13")
+else:
+    print("Ist etwas anderes")
+
+for i in range(0, 10, 2):
+    print(i)
+
+liste = ["apple", "beer", "python"]
+for i in liste:
+    print(i)
+
+x=0
+while x<100:
+    print(x)
+    x += 10
+
+# Type Casting
+t = 0
+t = int(t)
+t = float(t)
+t = str(t)
+
+# Gut zu wissen
+print( True > False)
+
+
+input()
