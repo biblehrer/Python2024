39dfe09cc2e061be6379de4007591c7efc4b1117
Stefan Gerstner <Stefan.Gerstner@bib.de>
2024-05-08 12:59:13 +0200

    Mehr zu Listen & Slices

--- listen.py
+++ listen.py
@@ -15,8 +15,35 @@ mix = [12, 15.4, "Brot", False, print, [1, 2, 3 ] ]
 
 print(liste)
 
-#y = liste[1:4:2]
 
+# Listen sind bei Reference und nicht bei Value übergeben
+liste1 = [1,2,99, 61,6,7,8]
+liste2 = liste1
+liste2 = liste1[:]
+liste1[0] = 9
+print(liste2[0])
+
+# Um eine die Werte einer Liste zu kopieren benutzen wir den Slice
+liste3 = liste1[:]
+print(liste3)
+
+# Beim for Loop definieren die Range wie folge range(start, end, step)
+# Dasselbe gilt auch für den slice
+start=0
+end=len(liste1)
+step=1
+
+for i in range(start, end, step):
+    v = liste1[i]
+
+liste4 = liste[start,end, step]
+
+# Mit dem Keyword in können wir überprüfen ob ein Element in einer Liste ist
+b = 5 not in liste2
+print(b)
+
+
+l = liste1[5:3:1]
 print(y)
 
 input("Done")
