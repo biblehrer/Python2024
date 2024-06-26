fcc5a5c61836548440969eb232c970dbec53d48d
Stefan Gerstner <Stefan.Gerstner@bib.de>
2024-06-26 12:51:55 +0200

    Talking about moduls

---
+++ imports.py
@@ -0,0 +1,42 @@
+#import math, sys
+#import sys
+
+#from math import *
+
+#import math as mathe
+
+#from math import pi as PIII 
+
+
+
+#pi = 3.14
+
+#print(math.pi)
+#print(pi)
+
+#mathe.sin(pi)
+
+
+
+def newStuff():
+    import math
+    print(math.pi)
+
+newStuff()
+
+try:
+    print(math.pi)
+except:
+    print("Fehler")
+
+
+pi = 3.14
+
+from math import pi
+
+
+
+print(pi)
+
+
+input("Done")
